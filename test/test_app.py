603cc3207fc87bc65c9e33dc043368585a4d5d54
shaniblau <blaushani02@gmail.com>
2023-07-16 15:10:40 +0300

    removed 1 unittest

--- test/test_app.py
+++ test/test_app.py
@@ -1,7 +1,3 @@
-from fastapi import UploadFile
-from fastapi.testclient import TestClient
-
-
 def test_sort_files(app_fixture):
     file_a = File(filename='file_a.jpg')
     file_b = File(filename="file_b.jpg")
@@ -9,16 +5,6 @@ def test_sort_files(app_fixture):
     expected = [file_a, file_b]
     result = app_fixture.sort_files(files)
     assert result == expected
-
-
-def test_create_single_file(app_fixture):
-    files = [("files", ('file_a.txt', open('file_a.txt', "rb"), "image/jpg")),
-             ("files", ('file_b.txt', open('file_b.txt', "rb"), "image/jpg"))]
-    app_fixture.create_single_file(files)
-    expected_file_content = b'ab'
-    with open('../../Images/file_jpg', 'rb') as file:
-        result = file.read()
-    assert expected_file_content == result
 
 
 class File:

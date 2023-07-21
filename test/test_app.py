3d2c1150aa91696ebe10e28b82885b375aeb7be1
shaniblau <blaushani02@gmail.com>
2023-07-21 11:07:35 +0300

    added testing for app module

--- test/test_app.py
+++ test/test_app.py
@@ -1,3 +1,18 @@
+import os
+from fastapi import UploadFile
+from fastapi.testclient import TestClient
+
+
+def test_create_upload_file_endpoint(app_fixture):
+    client = TestClient(app_fixture)
+    files = [
+        ("file_a.jpg", b"Test file content A"),
+        ("file_b.jpg", b"Test file content B"),
+    ]
+    response = client.post("/uploadfile", files=files)
+    assert response.status_code == 200
+
+
 def test_sort_files(app_fixture):
     file_a = File(filename='file_a.jpg')
     file_b = File(filename="file_b.jpg")
@@ -7,7 +22,31 @@ def test_sort_files(app_fixture):
     assert result == expected
 
 
+def test_create_single_file(app_fixture, mocker):
+    create_files()
+    files = [UploadFile(filename='file_a', file=open('/home/runner/work/sum-http-client/images/file_a', "rb")),
+             UploadFile(filename='file_a', file=open('/home/runner/work/sum-http-client/images/file_a', "rb"))]
+    mocker.patch('app.sing_file')
+    app_fixture.create_single_file(files)
+    expected = b'ab'
+    with open('/home/runner/work/sum-http-client/images/file.jpg', 'rb') as file:
+        result = file.read()
+    assert result == expected
+
+
 class File:
     def __init__(self, filename, content=''):
         self.filename = filename
         self.content = content
+
+
+def create_files():
+    if not os.path.exists('/home/runner/work/sum-http-client/images'):
+        os.makedirs('/home/runner/work/sum-http-client/images')
+    with open('/home/runner/work/sum-http-client/images/file_a', 'wb') as file:
+        file.write(b'a')
+    with open('/home/runner/work/sum-http-client/images/file_b', 'wb') as file:
+        file.write(b'b')
+
+
+

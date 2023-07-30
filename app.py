6926202f29192b1e7274752266a2b8e9ed3646f3
shaniblau <blaushani02@gmail.com>
2023-07-30 16:56:08 +0300

    editted ci-cd.yml file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import logging as log
 import os
 from typing import List
-from config import images_dir
+import config
 from fastapi import FastAPI, UploadFile
 
 from sign_file import execute
@@ -13,8 +13,8 @@ log.basicConfig(filename='files_created.log', filemode='a', level=log.INFO,
 
 @app.post("/uploadfile")
 async def create_upload_file(files: List[UploadFile]):
-    if not os.path.exists(images_dir):
-        os.mkdir(images_dir)
+    if not os.path.exists(config.images_dir):
+        os.mkdir(config.images_dir)
     await create_single_file(sort_files(files))
 
 
@@ -29,7 +29,7 @@ def sort_files(files: List[UploadFile]):
 async def create_single_file(files: List[UploadFile]):
     name = files[0].filename.split('_')[0]
     file_name = f'{name}.jpg'
-    path = os.path.join(images_dir, file_name)
+    path = os.path.join(config.images_dir, file_name)
     with open(path, 'ab') as file:
         for f in files:
             file.write(await f.read())

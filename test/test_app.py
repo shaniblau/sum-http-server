9bb085677daf23737141ca16eb6a5c5f87f52449
shaniblau <blaushani02@gmail.com>
2023-08-07 11:06:08 +0300

    organized code to dirs

--- test/test_app.py
+++ test/test_app.py
@@ -2,8 +2,10 @@
 # import pytest
 # from fastapi import UploadFile
 # from fastapi.testclient import TestClient
-#
-# from help_funcs import create_files, File
+
+from help_funcs import create_files, File
+
+
 #
 #
 # def test_create_upload_file_endpoint(app_fixture):
@@ -15,14 +17,14 @@
 #     assert response.status_code == 200
 #
 #
-# def test_sort_files(app_fixture):
-#     file_a = File(filename='file_a.jpg')
-#     file_b = File(filename="file_b.jpg")
-#     files = [file_b, file_a]
-#     expected = [file_a, file_b]
-#     result = app_fixture.sort_files(files)
-#     assert result == expected
-#
+def test_sort_files(app_fixture):
+    file_a = File(filename='file_a.jpg')
+    file_b = File(filename="file_b.jpg")
+    files = [file_b, file_a]
+    expected = [file_a, file_b]
+    result = app_fixture.sort_files(files)
+    assert result == expected
+
 #
 # @pytest.mark.asyncio
 # async def test_create_single_file(app_fixture, sign_fixture, mocker):

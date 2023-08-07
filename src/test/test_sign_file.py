2ffbd82ccc5f8b2519439b5a658d34fc744f1a0c
shaniblau <blaushani02@gmail.com>
2023-08-07 18:59:15 +0300

    adding sign_file tests

--- src/test/test_sign_file.py
+++ src/test/test_sign_file.py
@@ -1,11 +1,11 @@
 import builtins
 import os
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 
 
 def test_sign_should_call_create_encrypted_hash_once(sign_fixture, mock_create_encrypted_hash_fixture):
-    with patch('sign_file.sign.open',
-               side_effect=lambda file, mode: builtins.open(os.path.abspath(file), mode)) as mock_file:
+    mock_file = mock_open()
+    with patch('sign_file.open', mock_file):
         sign_fixture.sign('file.jpg')
     mock_create_encrypted_hash_fixture.assert_called_once()
 

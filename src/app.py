437925decc2a5b0f4abe44c2825850895dd867ca
shaniblau <blaushani02@gmail.com>
2023-08-07 14:39:54 +0300

    fixing tests

--- src/app.py
+++ src/app.py
@@ -11,16 +11,16 @@ from set_logger import extendable_logger
 
 app = FastAPI()
 date = datetime.now().strftime("%d_%m_%Y")
-created_logger = extendable_logger(f'../logs/files-created/{date}.log', log.INFO)
-error_logger = extendable_logger('../logs/errors.log', log.WARNING)
 
 
 @app.post("/uploadfile")
 async def create_upload_file(files: List[UploadFile]):
+    created_logger = extendable_logger(f'../logs/files-created/{date}.log', log.INFO)
+    error_logger = extendable_logger('../logs/errors.log', log.WARNING)
     try:
         if not os.path.exists(config.IMAGES_DIR):
             os.mkdir(config.IMAGES_DIR)
-        await create_single_file(sort_files(files))
+        await create_single_file(sort_files(files), created_logger)
     except Exception:
         error_logger.error(f'the files {files[0].filename}, {files[1].filename} were not combined')
 
@@ -32,7 +32,7 @@ def sort_files(files: List[UploadFile]):
         return [files[1], files[0]]
 
 
-async def create_single_file(files: List[UploadFile]):
+async def create_single_file(files: List[UploadFile], created_logger):
     name = files[0].filename.split('_')[0]
     file_name = f'{name}.jpg'
     path = os.path.join(config.IMAGES_DIR, file_name)

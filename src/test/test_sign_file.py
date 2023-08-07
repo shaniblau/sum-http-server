9dbfd40fff0fd6fdab58ab10e3b3c104818877a0
shaniblau <blaushani02@gmail.com>
2023-08-07 19:13:38 +0300

    adding sign_file tests

--- src/test/test_sign_file.py
+++ src/test/test_sign_file.py
@@ -12,33 +12,6 @@ def test_sign_should_call_create_encrypted_hash_once(sign_fixture, mock_create_e
 
 def test_create_encrypted_hash_should_call_create_sha512_once(sign_fixture, mock_create_sha512_fixture):
     iv = get_random_bytes(16)
-    with patch('sign_file.open'):
+    with patch('sign_file.open'), patch('sign_file.AES.new'):
         sign_fixture.create_encrypted_hash('file.jpg', iv)
     mock_create_sha512_fixture.assert_called_once()
-
-
-# def test_create_sha512(sign_fixture):
-#     # Test creating a SHA-512 hash
-#     hash_value = sign_fixture.create_sha512('test_file.txt')
-#     assert len(hash_value) == 64  # SHA-512 produces a 64-byte hash
-#
-#
-# def test_create_encrypted_hash(sign_fixture):
-#     # Test creating an encrypted hash
-#     file_path = 'test_file.txt'
-#     iv = b'\x00' * 16  # Replace with a valid IV
-#     encrypted_hash = sign_fixture.create_encrypted_hash(file_path, iv)
-#     assert len(encrypted_hash) == 64  # Assuming AES encryption produces a 64-byte output
-#
-#
-# def test_sign(tmp_path, sign_fixture):
-#     # Test signing a file
-#     file_path = os.path.join(tmp_path, 'test_file.txt')
-#     with open(file_path, 'wb') as f:
-#         f.write(b'This is a test file content')
-#     # Perform the signing operation
-#     sign_fixture.sign(file_path)
-#     # Assert that the file now contains the expected content
-#     with open(file_path, 'rb') as f:
-#         content = f.read()
-#         assert len(content) >= 16 + 64  # IV + encrypted hash

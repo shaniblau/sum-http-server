0134341bd1e15a986bc4180a1e706bf74eca6f03
shaniblau <blaushani02@gmail.com>
2023-08-07 17:47:42 +0300

    fixing tests

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -34,18 +34,18 @@ def test_sort_files_should_replace_file_a_and_file_b_locations_in_the_list(app_f
     assert result == expected
 
 
-# @pytest.mark.asyncio
-# async def test_create_single_file(app_fixture, mocker):
-#     create_files()
-#     images_dir = './'
-#     files = [
-#         UploadFile(filename='file_a.jpg', file=open('./file_a', "rb")),
-#         UploadFile(filename='file_b.jpg', file=open('./file_b', "rb")),
-#     ]
-#     mocker.patch('app.config.IMAGES_DIR', images_dir)
-#     mocker.patch('app.sign')
-#     await app_fixture.create_single_file(files)
-#     expected = b'ab'
-#     with open(os.path.join(images_dir, 'file.jpg'), 'rb') as file:
-#         result = file.read()
-#     assert result == expected
+@pytest.mark.asyncio
+async def test_create_single_file(app_fixture, mocker):
+    create_files()
+    images_dir = './'
+    files = [
+        UploadFile(filename='file_a.jpg', file=open('./file_a', "rb")),
+        UploadFile(filename='file_b.jpg', file=open('./file_b', "rb")),
+    ]
+    mocker.patch('app.config.IMAGES_DIR', images_dir)
+    mocker.patch('app.sign')
+    await app_fixture.create_single_file(files)
+    expected = b'ab'
+    with open(os.path.join(images_dir, 'file.jpg'), 'rb') as file:
+        result = file.read()
+    assert result == expected

9840fbca77b8cb186a9a3a6403262397f0d66d94
shaniblau <blaushani02@gmail.com>
2023-08-07 17:46:26 +0300

    fixing tests

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -20,19 +20,20 @@ def test_create_upload_file_should_log_error_and_response_not_200(app_fixture, l
     set_mockers(mocker, logger_fixture)
     client = TestClient(app_fixture.app)
     files = create_files()
-    response = client.post("uploadfile", files=files)
+    response = client.post("/", files=files)
     assert response != 200
     assert os.path.isfile('./logs/errors.log')
 
 
-# def test_sort_files_should_replace_file_a_and_file_b_locations_in_the_list(app_fixture):
-#     file_a = File(filename='file_a.jpg')
-#     file_b = File(filename="file_b.jpg")
-#     files = [file_b, file_a]
-#     expected = [file_a, file_b]
-#     result = app_fixture.sort_files(files)
-#     assert result == expected
-#
+def test_sort_files_should_replace_file_a_and_file_b_locations_in_the_list(app_fixture):
+    file_a = File(filename='file_a.jpg')
+    file_b = File(filename="file_b.jpg")
+    files = [file_b, file_a]
+    expected = [file_a, file_b]
+    result = app_fixture.sort_files(files)
+    assert result == expected
+
+
 # @pytest.mark.asyncio
 # async def test_create_single_file(app_fixture, mocker):
 #     create_files()

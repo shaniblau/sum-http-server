68d192e6f4f7b8e98296b5eddff4220413e343c9
shaniblau <blaushani02@gmail.com>
2023-08-27 13:58:00 +0300

    changed tests according to code changes

--- src/test/test_sign_file.py
+++ src/test/test_sign_file.py
@@ -4,5 +4,5 @@ from unittest.mock import patch, mock_open
 def test_sign_should_call_create_encrypted_hash_once(sign_fixture, mock_create_encrypted_hash_fixture):
     mock_file = mock_open()
     with patch('sign_file.open', mock_file):
-        sign_fixture.sign('file.jpg')
+        sign_fixture.digital_sign('file.jpg')
     mock_create_encrypted_hash_fixture.assert_called_once()
